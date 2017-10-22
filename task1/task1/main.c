3533b2591bca29cc4f727fd7206ff2e73a4966a3
Andrey A Molkov <molkov_aa@mail.ru>
2017-10-22 22:06:10 +0300

    first text

---
+++ task1/task1/main.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+include checkAge
+
+main1()
+{
+	printf("введите ваш возраст: ");
+	scanf("%d", &age);
+		checkAge ();
+		if (return==1)
+			printf("Access granted")
+		else
+			printf("Access denied")
+	return0;
+}
