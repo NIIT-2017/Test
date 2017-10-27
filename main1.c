125ccbbc41f55f7b6abfbda2f8e3ba0013aff4b8
SergeyPerepelkin <Seregaperepelmail@gmail.com>
2017-10-27 18:16:56 +0300

    Добавил файлы

---
+++ main1.c
@@ -0,0 +1,15 @@
+﻿#Include <stdio.h>
+#Include "task1.h"
+
+int main()
+{
+	int Age;
+		Printf("Enter your age/h");
+		scanf(%d, &Age);
+		CheckAge(int Age)
+			if CheckAge(Age) = 0 
+				Printf("Acces granted/n")
+			else
+				printf("Acces denied/n")
+		return 0;
+}
---
+++ task1.c
@@ -0,0 +1,7 @@
+﻿int CheckAge(int Age);
+{
+	if CheckAge(Age) >= 18
+		return 0
+	else
+		return 1;
+}
