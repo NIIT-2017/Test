0376599f09c27c89315397284e7de6aab3a3c6b5
Andrey A Molkov <molkov_aa@mail.ru>
2017-10-27 21:36:42 +0300

    final versiy

--- work/main1.c
+++ work/main1.c
@@ -1,17 +1,20 @@
-#include"task.h"
+#include"task1.h"
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-	printf("Enter your age : /n");
-	scanf_s("%", &age);
+	int age=0;
+	printf("Enter your age : \n");
+	scanf_s("%d", &age);
+	
+	
 	int checkAge(int age);
-	if (checkAge(age)==1)
+	if (checkAge(age) == 1)
 		printf("Access granted");
 	else
 		printf("Access denied");
 
-	_getchar();
+	//_getch();
 
 	return 0;
 }
