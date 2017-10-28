60d143e2a904d3919974200dca03f894feebd47b
MartianovAV <andrei.martianoff92@yandex.ru>
2017-10-28 18:37:33 +0300

    dz

---
+++ work-b19/main1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "task1.h"
+
+
+
+int main()
+{
+	int age;
+	printf("Enter your age: ");
+	scanf("%d", &age);
+	if (checkAge(age) == 1)
+		printf("Access granted\n");
+	else
+		printf("Access denied\n");
+	getch();
+	return 0;
+}
---
+++ work-b19/task1.c
@@ -0,0 +1,9 @@
+#include "task1.h"
+
+int checkAge(int age)
+{
+	if (age >= 18)
+		return 1;
+	else
+		return 0;
+}
