c9b6b371bb8670d80f43c4ce5e59eda73ff446d7
Andrey A Molkov <molkov_aa@mail.ru>
2017-10-29 13:33:04 +0300

    change scanf_s to scanf

--- work/main1.c
+++ work/main1.c
@@ -1,14 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include"task1.h"
 #include<stdio.h>
 //#include<conio.h>
+
 int main()
 {
 	int age=0;
 	printf("Enter your age : \n");
-	scanf_s("%d", &age);
+	scanf("%d", &age);
 	
 	
 	int checkAge(int age);
+	/*if (scanf("%d", &age) == 0)
+	{
+		printf("Data error");
+		return 1;
+	}*/
 	if (checkAge(age) == 1)
 		printf("Access granted");
 	else
