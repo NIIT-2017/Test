38dcdf77fb27ef0323c348c4cb507c7cbf35ef12
skvortsovss <skvortsovss@mail.ru>
2018-11-03 17:53:20 +0300

    rename main.cpp to main1.cpp

---
+++ src/main1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "task1.h"
+
+int main()
+{
+	int age = 0;
+	printf("enter your age:");
+	scanf_s("%d", &age);
+	if (ageCheck(age) == 0)
+	{
+		printf("Access denied");
+	}
+	else
+	{
+		printf("Access granted");
+	}
+		return 0;
+}
