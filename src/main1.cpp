a45494e2d550d5846e4db2c8d41469ee5a132171
Александр Садиков <sav1982@inbox.ru>
2018-11-16 22:12:32 +0300

    Добавили task

---
+++ src/main1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "task.h"
+
+int main()
+{
+	int i = 0;
+	printf("\nEnter your age: ");
+	scanf("%d", &i);
+	if (ageCheck(i) == 1)
+	{
+		printf("Access granted");
+	}
+	else
+	{
+		printf("Access denied");
+	}
+	
+	return 0;
+}
