91546a8fb6349862370da32c7b0583eec07f2528
sherboguslav <45122372+sherboguslav@users.noreply.github.com>
2018-11-17 17:24:54 +0300

    Just 4 test git

---
+++ test/include/task1.h
@@ -0,0 +1,6 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+int ageCheck(int age);
+
+#endif // !TASK1_H
---
+++ test/src/main1.cpp
@@ -0,0 +1,16 @@
+#include "task1.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	puts("What is your age?");
+	int age = 0;
+	scanf("%d", &age);
+	int result = ageCheck(age);
+	if (result == 1)
+		puts("Access granted");
+	else
+		puts("Access denied");
+	return 0;
+}
