6e42235aa088c3eeb9668c8c4ba31f77caf333e2
AlexandrMonakhov <zabuldigaa@gmail.com>
2018-11-17 10:22:55 +0300

    age изменено на Age

--- src/main1.cpp
+++ src/main1.cpp
@@ -3,9 +3,9 @@
 
 int main()
 {
-	int age = 0;
-	scanf("%d", &age);
-	if (age >= 18)
+	int Age = 0;
+	scanf("%d", &Age);
+	if (Age >= 18)
 		printf("Access granted");
 	else
 		printf("Access denied");
