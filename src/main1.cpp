e5f96d3f7e7604c8ab54530c2a462bd3102aa644
Shalagin Nikolay <qquikly@mail.ru>
2018-11-18 09:42:54 +0300

    new project v2

---
+++ src/main1.cpp
@@ -0,0 +1,11 @@
+#include "task1.h"
+
+int main()
+{
+	int age;
+	printf("How old are you? ");
+	scanf("%d", &age);
+	if (ageCheck(age) == 1) printf("Access granted\n");
+	else printf("Access denied\n");
+	return 0;
+}
