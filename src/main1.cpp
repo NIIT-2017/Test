0e4f5ae3e79e0c5993ad6578c739530c026875c7
AlexandrMonakhov <zabuldigaa@gmail.com>
2018-11-18 10:48:30 +0300

    Перезалил файлы

---
+++ src/main1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "task1.h"
+
+int main()
+{
+	int age = 0;
+	scanf("%d", &age);
+	if (ageCheck(age) == 1)
+		printf("Access granted");
+	else
+		printf("Access denied");
+	return 0;
+}
