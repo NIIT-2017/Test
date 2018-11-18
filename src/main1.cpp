d467c747b8cb85ad564a01a1dc5d7589a9dc47be
AlexandrMonakhov <zabuldigaa@gmail.com>
2018-11-18 09:16:23 +0300

    Final version

--- src/main1.cpp
+++ src/main1.cpp
@@ -3,9 +3,9 @@
 
 int main()
 {
-	int Age = 0;
-	scanf("%d", &Age);
-	if (Age >= 18)
+	int age = 0;
+	scanf("%d", &age);
+	if (ageCheck(int (age)) == 1)
 		printf("Access granted");
 	else
 		printf("Access denied");
