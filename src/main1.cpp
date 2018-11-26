dc2e7687b51ac8ccb77f93b94954c3b1bf0f24d4
sergone <medov777@yandex.ru>
2018-11-26 23:20:45 +0300

    изменения2

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include"task1.h"
-#include "pch.h"
+#include <conio.h>
 
 int main()
 {
 	int age = 0;
-printf("Enter your age: ");
-scanf_s("%d", &age);
-if (ageCheck(age) == 1) printf("Access granted\n");
-else printf("Access denied\n");
-return 0;
+	printf("Enter your age: ");
+	scanf_s("%d", &age);
+	if (ageCheck(age) == 1) printf("Access granted\n");
+	else printf("Access denied\n");
+	_getch();
+	return 0;
 }
