5199080dd6231c7c078d34249e42865fd58aa01c
lerkas <44975578+lerkas@users.noreply.github.com>
2018-11-27 13:53:08 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,33 +1,17 @@
 #include <stdio.h>
 #include "task1.h"
 
-void clean_stdin(void)
-{
-    int c=0;
-    do
-    {
-        c = getchar();
-    }
-
-    while (c != '\n' && c != EOF);
-}
-
 int main()
 {
     int age = 0;
 
-    while (1)
-    {
-        printf("How old are you?\n");
-        int result=scanf("%d", &age);
-                if (result == 0 || age<=0)
-                 {
-                    printf("Age is incorrect!\n");
-                    clean_stdin();
-                 }
-                else
-                    break;
-    }
-    printf(checkAge(age));
+    printf("How old are you?\n");
+    scanf("%d", &age);
+
+        if (checkAge(age) == 0)
+            printf("Access denied!\n");
+        if (checkAge(age) == 1)
+            printf("Access granted!\n");
+
     return 0;
 }
