6098478ff6470ff0b5025666347d4651c32361c7
sergone <medov777@yandex.ru>
2018-12-02 17:17:06 +0300

    исправление 4

--- src/main1.cpp
+++ src/main1.cpp
@@ -6,8 +6,10 @@ int main()
 {
 	int age = 0;
 	printf("Enter your age: ");
-	scanf_s("%d", &age);
-	if (ageCheck(age) == 1) printf("Access granted\n");
-	else printf("Access denied\n");
+	scanf("%d", &age);
+	if (ageCheck(age) == 1)
+		printf("Access granted\n");
+	else 
+		printf("Access denied\n");
 	return 0;
 }
