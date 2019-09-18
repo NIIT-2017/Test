94f328d0e30dd93f8f3d771e7c2c02e26ed565d2
Windows 7 <student7@std.nnov.corp.local>
2019-09-18 12:26:02 +0300

    done task

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "task1.h"
+#include <iostream>
+using namespace std;
+int main(){
+	int a;
+	cin >> a;
+	if (ageCheck(a))
+		cout << "Access granted" << endl;
+	else
+		cout << "Access denied" << endl;
+}
---
+++ src/task1.cpp
@@ -0,0 +1,7 @@
+#include "task1.h"
+int ageCheck(int age){
+	if (age < 18)
+		return 0;
+	else
+		return 1;
+}
