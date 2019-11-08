b54747e03d2b98c81febb95b6bd457e3905150f4
AzamatMuratshin <azketc4@gmail.com>
2019-11-08 22:21:01 +0300

    =Test_ready_b38

---
+++ src/task2.cpp
@@ -0,0 +1,15 @@
+#include “task1.h“
+#include <stdio.h> 
+
+int main ()
+{
+	int age 0;
+		printf(“How old are you?  “);
+		scanf(“%d“,&age);
+	int result=ageCheck(age);
+		if(result==1)
+			printf(“Access granted!\n“);
+		else
+			printf(“Access denied!\n“);
+	return 0;
+}
