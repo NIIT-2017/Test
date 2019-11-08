1573407c03f2059200c9b77cceec5de433693c92
ekaryagin <ekaryagin@gmail.com>
2019-11-08 23:36:03 +0300

    Test completed

---
+++ src/main1.cpp
@@ -0,0 +1,14 @@
+#include "task1.h"
+#include <stdio.h>
+
+int main()
+{
+	int age = 0;
+	printf("How old are you: ");
+	scanf_s("%d", &age);
+	if (ageCheck(age))
+		printf("Access sucsesful");
+	else
+		printf("Access denied");
+	return 0;
+}
