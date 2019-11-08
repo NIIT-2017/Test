9102a5d2c951de143b7812a085fc4b18ebe5f374
AzamatMuratshin <azketc4@gmail.com>
2019-11-08 21:19:39 +0300

    =Ready3

--- src/task2.cpp
+++ src/task2.cpp
@@ -4,12 +4,12 @@
 int main ()
 {
 	int age 0;
-		printf("How old are you?  ");
-		scanf("%d",&age);
+		printf(“How old are you?  “);
+		scanf(“%d“,&age);
 	int result=ageCheck(age);
 		if(result==1)
-			printf(“Access granted! \n”);
+			printf(“Access granted!\n“);
 		else
-			printf(“Access denied!\n”);
+			printf(“Access denied!\n“);
 	return 0;
 }
