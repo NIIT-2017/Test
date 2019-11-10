4906774618c7057804f2e6614ed42d7103efc2c4
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2019-11-10 22:03:39 +0300

    fin test

---
+++ src/main1.cpp
@@ -0,0 +1,15 @@
+
+#include "task1.h"
+#include <stdio.h>
+
+int main()
+{
+	int age = 0;
+	printf("How old are you: ");
+	scanf("%d", &age);
+	if (ageCheck(age))
+		printf("Access sucsesful");
+	else
+		printf("Access denied");
+	return 0;
+}
