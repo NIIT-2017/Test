146ed168ad816a5e33ffca647a9fd52af83c1881
Nikolay Babichev <babnikolay@yahoo.com>
2019-11-10 13:37:11 +0300

    New files were added

---
+++ src/main1.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "task1.h"
+#include <stdio.h>
+#include <clocale>	// для перехода на русский язык
+
+int main() {
+
+	int age = 0;
+
+	setlocale(LC_ALL, "Russian");
+
+	printf("ВВести возраст: ");
+	scanf("%d", &age);
+	int result = ageCheck(age);
+	if (result == 0)
+		printf("Доступ разрешён\n");
+	else
+		printf("Доступ запрещён\n");
+
+	return 0;
+}
