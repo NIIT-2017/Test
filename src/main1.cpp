5cc7717c1aa58fbb313a46cfb4c804bd8809870d
IlyaKruglov <ilyakruglov@list.ru>
2019-11-30 18:04:25 +0300

    tst3

---
+++ src/main1.cpp
@@ -0,0 +1,14 @@
+#include "task1.h"
+#include <stdio.h>
+
+int main ()
+{
+	int age=0;
+	printf ("How old are you? ");
+	scanf ("%d", &age);
+	if (result==1)
+		printf ("Access garanted!\n");
+	else
+		printf ("Access denied!\n");
+	return 0;
+}
