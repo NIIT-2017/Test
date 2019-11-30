11697f800c4511c0d991833a9a489f2acfdced40
Syatojkin_Evgenij <Syatojkin_Evgenij@mail.ru>
2019-11-30 17:31:02 +0300

    test

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define _CRT_SECURE_NO_WARNINGS
 #include "task1.h"
 #include <stdio.h>
@@ -13,19 +12,5 @@ int main()
 		printf("Access garanted!\n");
 	else
 		printf("Access denied!\n");
-=======
-#include "task1.h"
-#include <stdio.h>
-
-int main ()
-{
-	int age=0;
-	printf ("How old are you? ");
-	scanf ("%d", &age);
-	if (result==1)
-		printf ("Access garanted!\n");
-	else
-		printf ("Access denied!\n");
->>>>>>> 468343f434caefa91f4c6f89271cd5858281a508
 	return 0;
 }
